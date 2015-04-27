a84e599d638f9f2cfdf793517bf215a10af7361e
Igor Santos <igorsantos07@gmail.com>
2015-04-27 02:52:00 -0300

    Adding submodules to deploy hook

--- config/post-receive.githook.sh
+++ config/post-receive.githook.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+echo "========= LET THE PARTY BEGIN =========="
 GIT_WORK_TREE=/var/www/protoboard git checkout -f
 cd /var/www/protoboard
+echo "========= UPDATING SUBMODULES =========="
+git submodule init
+git submodule update
 echo "=========== CHECKOUT IS DONE ==========="
 npm install
 echo "====== NODE PACKAGES ARE UPDATED ======="
