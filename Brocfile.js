639977f6729e326a3baa29907d0cb266f6f811e3
Igor Santos <igorsantos07@gmail.com>
2015-05-31 18:47:34 -0300

    improving Brocfile organization; skipping minification of source files in development as all of them get rid of sourcemaps

--- Brocfile.js
+++ Brocfile.js
@@ -1,4 +1,7 @@
 /* global require, module */
+var ENV = process.env.BROCCOLI_ENV || 'development';
+var isPROD = (ENV == 'production');
+
 var autoprefixer = require('broccoli-autoprefixer');
 var htmlmin = require('broccoli-htmlmin');
 var cssmin  = require('broccoli-clean-css');
@@ -11,22 +14,35 @@ var funnel   = require('broccoli-funnel');
 var merge    = require('broccoli-merge-trees');
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp({
-  stylusOptions: {
-    sourceMap: false //useless? how to use Stylus sourcemaps?
-  }
-});
 
-// Use `app.import` to add additional libraries to the generated output files.
-//
+var options = {
+  stylus: {},
+  autoprefixer: {
+    browsers: ['> 1%', 'last 2 versions', 'Firefox ESR'], //default
+    cascade: true
+  },
+  htmlmin: {
+    conditionals: true
+  },
+  cssmin: {
+    keepSpecialComments: (ENV == 'development')? '*' : '0'
+  },
+  jsmin: {},
+  imgmin: {
+    interlaced: true,
+    optimizationLevel: 3,
+    progressive: true,
+    lossyPNG: false
+  },
+  gzip: { extensions: ['js', 'css'] }
+};
+
+
+var app = new EmberApp({ stylusOptions: options.stylus });
+
 // If you need to use different assets in different environments, specify an
 // object as the first parameter. That object's keys should be the environment
 // name and the values should be the asset to use in that environment.
-//
-// If the library that you are including contains AMD or ES6 modules that you
-// would like to import into your application please specify an object with the
-// list of modules as keys along with the exports of each module as its value.
-
 app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
 app.import('bower_components/bootswatch/sandstone/bootstrap.min.css');
 app.import('bower_components/bootswatch/fonts/glyphicons-halflings-regular.woff2', {destDir: 'fonts'});
@@ -41,40 +57,22 @@ var progressHelper = merge([
   funnel('app/styles', { files: ['pace.css'], destDir: 'assets/pace' })
 ]);
 
-completeTree = merge([mainTree, progressHelper]);
-
-var options = {
-  autoprefixer: {
-    browsers: ['> 1%', 'last 2 versions', 'Firefox ESR'], //default
-    cascade: false
-  },
-  htmlmin: {
-    conditionals: true
-  },
-  cssmin: {},
-  jsmin: {},
-  imgmin: {
-    interlaced: true,
-    optimizationLevel: 3,
-    progressive: true,
-    lossyPNG: false
-  },
-  gzip: { extensions: ['js', 'css'] }
-};
+finalTree = merge([mainTree, progressHelper]);
 
 //TODO: optimize trees with funnel so minification won't pass by unrelated files
 //FIXME: remove useless images from build (img/outros)
-finalTree =
-  //gzip(
-    htmlmin(
-      jsmin(
-        cssmin(
-          imgmin(
-            autoprefixer(completeTree, options.autoprefixer),
-          options.imgmin),
-        options.cssmin),
-      options.jsmin),
-    options.htmlmin);
-  //options.gzip);
+finalTree = autoprefixer(finalTree, options.autoprefixer);
+if (isPROD) {
+  //no need to compress images in development
+  finalTree = imgmin(finalTree, options.imgmin);
+  //unable to compress and leave sourcemap comment: github.com/jakubpawlowicz/clean-css/issues/589
+  finalTree = cssmin(finalTree, options.cssmin);
+  //misteriously, output.comments=true will still stripe out sourcemap comment in the end
+  //FIXME: there's no support for CoffeeScript sourcemaps yet: github.com/kimroen/ember-cli-coffeescript/issues/7
+  finalTree = jsmin(finalTree, options.jsmin);
+}
+finalTree = htmlmin(finalTree, options.htmlmin);
+//won't work without headers, I guess
+//finalTree = gzip(finalTree, options.gzip);
 
 module.exports = finalTree;
